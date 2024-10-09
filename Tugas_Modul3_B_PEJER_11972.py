78e3ba1e571501539c074359ce1bba026367f3da
pejer28 <220711972@students.uajy.ac.id>
2024-10-09 15:44:34 +0700

    Update Tugas_Modul3_B_PEJER_11972.py

--- Tugas_Modul3_B_PEJER_11972.py
+++ Tugas_Modul3_B_PEJER_11972.py
@@ -82,8 +82,7 @@ if uploaded_file is not None:
     st.write("<h3 style='text-align: center; color: #0073e6;'>Data yang diupload :< /h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'C:\Users\pejer\OneDrive\Desktop\UAJY HUB\BISMILLAH S5 UAJY\00 - MATA KULIAH\Pembelejaran Mesin dan Pembelajaran Mendalam - B\praktek\Supervised Learning (Praktek)\Model Suprevised Learning (Praktek)\Tugas3_B_11972'
-    model_path = os.path. join(model_directory, r'SVR_IPK_model.pkl')
+    model_path = r'SVR_IPK_model.pkl'
 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as f:
